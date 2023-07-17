021fd63df84ff2c0aef42b6a17b835cc56b145db
Alexander Hirdman <88927248+ahirdman@users.noreply.github.com>
2023-07-17 16:28:29 +0200

    RTK Setup
    Setup basic RTK infrastrucutre

---
+++ src/store/user/user.slice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import type { User, UserState } from './user.interface';
+
+const initialState: UserState = {
+  user: null,
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    signIn: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+    },
+    signOut: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { signIn, signOut } = userSlice.actions;
+
+export default userSlice.reducer;
