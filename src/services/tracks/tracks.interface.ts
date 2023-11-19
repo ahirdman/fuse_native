6405947c94fce55f20bd112dd879249a9b3892a0
Alexander Hirdman <126080861+AlexHird@users.noreply.github.com>
2023-11-19 17:56:08 +0100

    Track view

--- src/services/tracks/tracks.interface.ts
+++ src/services/tracks/tracks.interface.ts
@@ -1,61 +1,57 @@
-import { z } from 'zod';
+export interface UserTracksReq {
+  market?: string | undefined;
+  limit?: number | undefined;
+  offset: number;
+}
 
-const albumImageDTO = z.object({
-  url: z.string().url(),
-  height: z.number().nullable(),
-  width: z.number().nullable(),
-});
+export interface SpotifyTrack {
+  id: string;
+  addedAt?: string | undefined;
+  artist?: string | undefined;
+  albumCovers: SpotifyAlbumImage[];
+  albumName: string;
+  name: string;
+  explicit: boolean;
+  duration: number;
+}
 
-const artistDTO = z.object({
-  name: z.string(),
-});
+/*
+ * Data Transfer Objects
+ * */
 
-const trackDTO = z.object({
-  name: z.string(),
-  album: z.object({
-    images: z.array(albumImageDTO),
-    name: z.string(),
-  }),
-  artists: z.array(artistDTO),
-  id: z.string(),
-});
+export interface UserSavedTracksDto {
+  href: string;
+  limit: number;
+  next: string;
+  offset: number;
+  previous: string;
+  total: number;
+  items: UserSavedTrackDto[];
+}
 
-const track = z.object({
-  mainArtist: z.string().optional(),
-  albumImageUrl: z.string().optional(),
-  albumName: z.string(),
-  name: z.string(),
-  id: z.string(),
-});
+export interface UserSavedTrackDto {
+  added_at: string;
+  track: SpotifyTrackDto;
+}
 
-export type Track = z.infer<typeof track>;
-export type TrackDTO = z.infer<typeof trackDTO>;
+interface SpotifyTrackDto {
+  name: string;
+  album: {
+    images: SpotifyAlbumImage[];
+    name: string;
+  };
+  artists: SpotifyArtistDto[];
+  id: string;
+  explicit: boolean;
+  duration_ms: number;
+}
 
-const savedTrackDTO = z.object({
-  added_at: z.string().datetime(),
-  track: trackDTO,
-});
-const savedTrack = savedTrackDTO.omit({ track: true }).extend({ track });
+interface SpotifyAlbumImage {
+  url: string;
+  height: number | null;
+  width: number | null;
+}
 
-export type SavedTrackDTO = z.infer<typeof savedTrackDTO>;
-export type SavedTrack = z.infer<typeof savedTrack>;
-
-export const userTracksDTO = z.object({
-  href: z.string().url(),
-  limit: z.number(),
-  next: z.string().url(),
-  offset: z.number(),
-  previous: z.string().url(),
-  total: z.number(),
-  items: z.array(savedTrackDTO),
-});
-
-export type UserTracksDTO = z.infer<typeof userTracksDTO>;
-
-const userTracksReq = z.object({
-  market: z.string().optional(),
-  limit: z.number().default(20).optional(),
-  offset: z.number().default(0),
-});
-
-export type UserTracksReq = z.infer<typeof userTracksReq>;
+interface SpotifyArtistDto {
+  name: string;
+}
