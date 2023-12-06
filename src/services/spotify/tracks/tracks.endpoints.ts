0d064f96808a16d2c5f70ff15e36b734a2647d71
Alexander Hirdman <126080861+AlexHird@users.noreply.github.com>
2023-12-06 21:09:32 +0100

    Add error handling
    Extra:
    
    - Refactor Tracks List
    - Add optional tagged state in TrackRow

--- src/services/spotify/tracks/tracks.endpoints.ts
+++ src/services/spotify/tracks/tracks.endpoints.ts
@@ -3,6 +3,7 @@ import { spotifyApi } from "../spotify.api";
 import type {
 	SpotifyTrack,
 	UserSavedTracksDto,
+	UserSavedTracksRes,
 	UserTracksReq,
 } from "./tracks.interface";
 
@@ -14,23 +15,29 @@ export const tracksApi = spotifyApi.injectEndpoints({
 			query: ({ limit, offset }: UserTracksReq) =>
 				`/me/tracks?offset=${offset}&limit=${limit}`,
 			keepUnusedDataFor: 600,
-			transformResponse: (response: UserSavedTracksDto): SpotifyTrack[] => {
-				return response.items.map((item) => ({
-					addedAt: item.added_at,
-					id: item.track.id,
-					artist: item.track.artists[0]?.name,
-					albumCovers: item.track.album.images,
-					albumName: item.track.album.name,
-					name: item.track.name,
-					explicit: item.track.explicit,
-					duration: item.track.duration_ms,
-				}));
+			transformResponse: (response: UserSavedTracksDto): UserSavedTracksRes => {
+				return {
+					...response,
+					items: response.items.map((item) => ({
+						addedAt: item.added_at,
+						id: item.track.id,
+						artist: item.track.artists[0]?.name,
+						albumCovers: item.track.album.images,
+						albumName: item.track.album.name,
+						name: item.track.name,
+						explicit: item.track.explicit,
+						duration: item.track.duration_ms,
+					})),
+				};
 			},
 			serializeQueryArgs: () => {
 				return GET_SPOTIFY_TRACKS_QUERY_ID;
 			},
-			merge: (currentCache, newItems): SpotifyTrack[] => {
-				return [...currentCache, ...newItems];
+			merge: (currentCache, newItems): UserSavedTracksRes => {
+				return {
+					total: currentCache.total,
+					items: [...currentCache.items, ...newItems.items],
+				};
 			},
 			forceRefetch: ({ currentArg, previousArg }) => {
 				return currentArg?.offset !== previousArg?.offset;
