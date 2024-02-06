4fe8482aa5f774c08b463630ae983cd0da609457
Alexander Hirdman <88927248+ahirdman@users.noreply.github.com>
2024-02-06 22:46:49 +0100

    Update to Expo 50 and switch to Bun

--- __mocks__/native.server.ts
+++ __mocks__/native.server.ts
@@ -1,9 +1,7 @@
 import { setupServer } from 'msw/native';
 
-import { handlers } from './handlers';
-
 export function initMocks() {
-  const nativeServer = setupServer(...handlers);
+  const nativeServer = setupServer();
   nativeServer.listen({ onUnhandledRequest: 'bypass' });
-  nativeServer.printHandlers();
+  nativeServer.listHandlers();
 }
