30e2126cd36d9b7a93622a361d81bc3a170ef0d0
Alexander Hirdman <88927248+ahirdman@users.noreply.github.com>
2024-05-29 21:42:28 +0200

    Switch image background to Skia

--- src/config/config-test.ts
+++ src/config/config-test.ts
@@ -2,7 +2,7 @@ import type { EnvironmentConfig } from '.';
 
 export const applicationConfiguration: EnvironmentConfig = {
   supabase: {
-    url: 'http://127.0.0.1:54321',
+    url: 'http://192.168.0.34:54321',
     anonKey:
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
   },
